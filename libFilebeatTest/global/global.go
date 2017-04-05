23d9b11844a1274bc4744693e3b615ed3154d1c8
Thomas Delacour <thomas.delacour@mongodb.com>
2017-04-05 16:08:41 -0400

    trimmed globals

--- libFilebeatTest/global/global.go
+++ libFilebeatTest/global/global.go
@@ -1,7 +1,5 @@
 package global
 
 const (
-	MmsGroupId string = "dummyUser"
-	MmsApiKey  string = "dummyPass"
-	Url        string = "http://localhost:8081/filebeatTest"
+	Url string = "http://localhost:8081/filebeatTest"
 )
